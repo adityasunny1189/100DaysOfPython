82c501fa1a797d1c176eec79eaac66f247debddd
adityasunny1189 <adityapathak1189@gmail.com>
2021-02-27 18:32:45 +0530

    day 1

---
+++ day1.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+print("Hello" + " " + "Aditya")
+
+print("Hello" + " " + input("Enter your name: "))
+
+print(len(input("Name: ")))
+
+name = input("Enter username: ")
+length = len(name)
+print(name) 
+print(length)
+
+#Project Day 1
+
+print("Welcome to band name generator")
+city_name = input("Enter the name of city you grew up in? ")
+pet_name = input("Enter your pet name? ")
+name_of_band = city_name + " " + pet_name
+print("Your band name is: " + name_of_band)
