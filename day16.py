99afa7e2e94024956d7bf4dfe29a81bd865fb181
adityasunny1189 <adityapathak1189@gmail.com>
2021-03-14 20:58:55 +0530

    Day 16

--- day16.py
+++ day16.py
@@ -1,21 +1,25 @@
 
-# from turtle import Turtle, Screen
+from turtle import *
 
-# timmy = Turtle()
-# timmy.shape("turtle")
-# timmy.color("red")
-# timmy.forward(100)
-# timmy.right(1)
-# timmy.backward(100)
-# timmy.left(1)
-# timmy.backward(100)
-# my_screen = Screen()
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.fillcolor("blue")
+timmy.forward(100)
+timmy.right(90)
+timmy.backward(100)
+timmy.left(90)
+timmy.backward(100)
+timmy.right(90)
+timmy.forward(100)
+my_screen = Screen()
 
-# my_screen.exitonclick()
+my_screen.exitonclick()
 
-from prettytable import PrettyTable
+# from prettytable import PrettyTable
 
-table = PrettyTable()
+# table = PrettyTable()
 
-table.add_column("Names", ["Aditya", "Amit", "Adarsh"])
-table.add_column("CGPA", [8, 9, 7])
+# table.add_column("Names", ["Aditya", "Amit", "Adarsh"])
+# table.add_column("CGPA", [8, 9, 7])
+
