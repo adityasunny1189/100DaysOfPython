79e4477480841bac6390b8326bb225fac1cc9085
adityasunny1189 <adityapathak1189@gmail.com>
2021-03-17 07:02:21 +0530

    Day 21

---
+++ day20/snake.py
@@ -0,0 +1,53 @@
+from turtle import Turtle
+
+POSITIONS = [(0,0), (-20,0), (-40,0)]
+MOVE_STEP = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+	def __init__(self):
+		self.segments = []
+		self.create_snake()
+		self.head = self.segments[0]
+
+	def create_snake(self):
+		for position in POSITIONS:
+			self.add_segment(position)
+
+	def add_segment(self, position):
+		segment = Turtle("square")
+		segment.color("red")
+		segment.penup()
+		segment.goto(position)
+		self.segments.append(segment)
+
+	def extend(self):
+		self.add_segment(self.segments[-1].position())
+
+	def move(self):
+		for seg_num in range(len(self.segments) - 1, 0, -1):
+			x_cor = self.segments[seg_num - 1].xcor()
+			y_cor = self.segments[seg_num - 1].ycor()
+			self.segments[seg_num].goto(x_cor, y_cor)
+		self.segments[0].forward(MOVE_STEP)
+
+	def up(self):
+		if self.segments[0].heading() != DOWN:
+			self.segments[0].setheading(UP)
+
+	def down(self):
+		if self.segments[0].heading() != UP:
+			self.segments[0].setheading(DOWN)
+
+	def left(self):
+		if self.segments[0].heading() != RIGHT:
+			self.segments[0].setheading(LEFT)
+
+	def right(self):
+		if self.segments[0].heading() != LEFT:
+			self.segments[0].setheading(RIGHT)
---
+++ day20/snake_game.py
@@ -0,0 +1,48 @@
+from turtle import Screen
+from scoreboard import ScoreBoard 
+from snake import Snake
+from food import Food
+import time
+
+scoreboard = ScoreBoard()
+screen = Screen()
+snake = Snake()
+food = Food()
+screen.bgcolor("black")
+screen.setup(height = 600, width = 600)
+screen.title("Snake Game")
+screen.tracer(0)
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_not_over = True
+
+while game_not_over:
+	screen.update()
+	time.sleep(.1)
+	snake.move()
+
+	# Collision with Food
+	if snake.head.distance(food) < 15:
+		food.refresh()
+		scoreboard.increment()
+		snake.extend()
+
+	# Collision with Wall
+	if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+		game_not_over = False
+		scoreboard.game_over()
+
+	# Collision with Tail
+	for segment in snake.segments:
+		if snake.head == segment:
+			continue
+		if snake.head.distance(segment) < 10:
+			game_not_over = False
+			scoreboard.game_over()
+
+screen.exitonclick()
