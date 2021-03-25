9809b5c9fa6a1fe8a29c0498171233503281d896
adityasunny1189 <adityapathak1189@gmail.com>
2021-03-25 19:06:15 +0530

    Day 24

--- day24/states name game/main.py
+++ day24/states name game/main.py
@@ -6,24 +6,31 @@ screen.setup(height = 500, width = 750)
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
-turtle.shape(image) 
-turtle.penup()
-turtle.tracer(0)
-game_not_over = True 
+turtle.shape(image)
 
 # Get states name file
 states = pandas.read_csv("50_states.csv")
 states_name = states.state.to_list()
 states_count = 0
+gussed_state = []
 
+while states_count < 50:
+	answer_imput = screen.textinput(title = f"{states_count}/{len(states.state)} the state", prompt = "what's another state name").title()
+	if answer_imput == "Exit":
+		break
 
-while game_not_over:
-	answer_imput = screen.textinput(title = f"{states_count}/{len(states.state)} the state", prompt = "what's another state name")
 	if answer_imput in states_name:
 		states_count += 1
-		x_cor = states[states.state == answer_imput].x.to_list()[0]
-		y_cor = states[states.state == answer_imput].y.to_list()[0]
-		turtle.goto(x_cor, y_cor)
-		turtle.write(answer_imput)
+		gussed_state.append(answer_imput)
+		state_data = states[states.state == answer_imput]
+		t = turtle.Turtle()
+		t.hideturtle()
+		t.penup()
+		t.goto(int(state_data.x), int(state_data.y))
+		t.write(answer_imput)
 
-screen.exitonclick()
+
+# Store the left over state in a csv file
+left_states = [s for s in states_name if s not in gussed_state]
+states_not_guessed = pandas.DataFrame(left_states)
+states_not_guessed.to_csv("states_not_guessed.csv")
