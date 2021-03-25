633ad5e843e73224c00c767c1f2f8e442a3421d4
adityasunny1189 <adityapathak1189@gmail.com>
2021-03-25 19:07:13 +0530

    Day 26

---
+++ day26/main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+window = Tk()
+window.minsize(height = 600, width = 800)
+window.title("My First GUI")
+
+def button_clicked():
+	user_input_text = user_input.get()
+	heading_1.config(text = user_input_text)
+
+heading_1 = Label(text = "Heading 1", font = ("Arial", 40, "bold"))
+heading_1.pack()
+
+button = Button(text = "click me", command = button_clicked)
+button.pack()
+
+user_input = Entry()
+user_input.pack()
+
+
+window.mainloop()
+
+
+
+
+# def add(*args):
+# 	for n in args:
+# 		print(n)
+
+# add(1,2,3)
+# add(1,2,3,4,5)
