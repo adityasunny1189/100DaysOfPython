9be087d00d42908f144e4f31a636eafe7328e23e
adityasunny1189 <adityapathak1189@gmail.com>
2021-04-30 17:15:49 +0530

    eg and random files

---
+++ random/crc.py
@@ -0,0 +1,9 @@
+divident = input('Enter a input: ')
+divisor = input('Enter a divisor: ')
+
+len_of_divisor = len(divisor)
+
+for i in range(len_of_divisor - 1):
+    divident += '0'
+
+len_of_divident = len(divident)
